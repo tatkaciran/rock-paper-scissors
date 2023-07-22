db2c3d58575489199480927ac339b970ac808d2d
batuacar <90128054+batuacar@users.noreply.github.com>
2023-07-22 15:07:40 +0300

    implement getPlayerSelection
    Implement the 'getPlayerSelection' function that allows the player to make a choice via a prompt and finds the index number of that choice from the choices

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const choices = ['rock', 'paper', 'scissors']
 function getComputerChoice() {
     return Math.floor(Math.random() * 3)
 }
+
+function getPlayerSelection() {
+    const choice = prompt('Select Rock, Paper or Scissors', 'paper')
+    const lowercaseChoice = choice.toLowerCase()
+    return choices.findIndex(value => {
+        return value === lowercaseChoice
+    })
+}
